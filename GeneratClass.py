802787305fbe57fcc187f7192cca4ef4fb2429f4
Yassine Taya <ytaya@e1r3p5.1337.ma>
2022-08-06 05:46:01 +0100

    modifiy GeneratClass.py

--- GeneratClass.py
+++ GeneratClass.py
@@ -7,35 +7,35 @@ import sys
 if (len( sys.argv ) > 1):
     className = sys.argv[1]
 # generate header file
-    with open(className.capitalize() + '.hpp', 'w') as f:
+    with open(className + '.hpp', 'w') as f:
         headerFile = "#ifndef " + className.upper() + "_CLASS\n"
         headerFile += "#define " + className.upper() + "_CLASS\n\n"
         headerFile += "#include <iostream>\n\n"
-        headerFile += "class " + className.capitalize() + "\n"
+        headerFile += "class " + className + "\n"
         headerFile += "{\n"
         headerFile += "public:\n"
-        headerFile += "\t" + className.capitalize() + "();\n"
-        headerFile += "\t" + "~" + className.capitalize() + "();\n"
-        headerFile += "\t" + className.capitalize() + "(const " + className.capitalize() + " &" + className[0].lower() + ");\n"
-        headerFile += "\t" + className.capitalize() + " & operator =" + "(const " + className.capitalize() + " &" + className[0].lower() + ");\n"
+        headerFile += "\t" + className + "();\n"
+        headerFile += "\t" + "~" + className + "();\n"
+        headerFile += "\t" + className + "(const " + className + " &" + className[0].lower() + ");\n"
+        headerFile += "\t" + className + " & operator =" + "(const " + className + " &" + className[0].lower() + ");\n"
         headerFile += "private: \n\n"
         headerFile += "}; \n\n"
         headerFile += "#endif // !" + className.upper() + "_CLASS \n"
         f.write(headerFile)
 # generate cpp file
-    with open(className.capitalize() + '.cpp', 'w') as f:
-        cppFile = "#include \"" + className.capitalize() + ".hpp\"\n\n"
-        cppFile += className.capitalize() + "::" + className.capitalize() + "(){\n"
+    with open(className + '.cpp', 'w') as f:
+        cppFile = "#include \"" + className + ".hpp\"\n\n"
+        cppFile += className + "::" + className + "(){\n"
         cppFile += "\t" + "std::cout << \"Default constructor called\" << std::endl;\n"
         cppFile += "} \n\n"
-        cppFile += className.capitalize() + "::~" + className.capitalize() + "(){\n"
+        cppFile += className + "::~" + className + "(){\n"
         cppFile += "\t" + "std::cout << \"Destructor called\" << std::endl;\n"
         cppFile += "} \n\n"
-        cppFile += className.capitalize() + "::" + className.capitalize() + "(const " + className.capitalize() + " &" + className[0].lower() + "){\n"
+        cppFile += className + "::" + className + "(const " + className + " &" + className[0].lower() + "){\n"
         cppFile += "\t" + "std::cout << \"Copy constructor called\" << std::endl;\n"
         cppFile += "\t *this = " + className[0].lower() + ";\n"
         cppFile += "} \n\n"
-        cppFile += className.capitalize() + " & "  + className.capitalize() + "::operator =" + "(const " + className.capitalize() + " &" + className[0].lower() + "){\n"
+        cppFile += className + " & "  + className + "::operator =" + "(const " + className + " &" + className[0].lower() + "){\n"
         cppFile += "\t" + "std::cout << \"Copy assignment operator called\" << std::endl;\n"
         cppFile += "\tif(this != &" + className[0].lower() + ")\n"
         cppFile += "\t{ \n"
