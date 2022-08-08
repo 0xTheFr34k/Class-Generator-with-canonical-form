e50bedeb1b1c8e97ece644f056091b1de8044250
BleedTheFreak <tayayassine6@live.fr>
2022-08-08 12:10:07 +0100

    multiple class arguments

--- GeneratClass.py
+++ GeneratClass.py
@@ -1,47 +1,47 @@
 #!/usr/bin/python
 
-# Generate class with canonical form
-
 import sys
 
+# Generate class with canonical form
+
+def GenerateClass(list):
+    for value in list:
+        with open(value + '.hpp', 'w') as f:
+            headerFile = "#ifndef " + value.upper() + "_CLASS\n"
+            headerFile += "#define " + value.upper() + "_CLASS\n\n"
+            headerFile += "#include <iostream>\n\n"
+            headerFile += "class " + value + "\n"
+            headerFile += "{\n"
+            headerFile += "public:\n"
+            headerFile += "\t" + value + "();\n"
+            headerFile += "\t" + "~" + value + "();\n"
+            headerFile += "\t" + value + "(const " + value + " &" + value[0].lower() + ");\n"
+            headerFile += "\t" + value + " & operator =" + "(const " + value + " &" + value[0].lower() + ");\n"
+            headerFile += "private: \n\n"
+            headerFile += "}; \n\n"
+            headerFile += "#endif // !" + value.upper() + "_CLASS \n"
+            f.write(headerFile)
+    for value in list:
+        with open(value + '.cpp', 'w') as f:
+            cppFile = "#include \"" + value + ".hpp\"\n\n"
+            cppFile += value + "::" + value + "(){\n"
+            cppFile += "\t" + "std::cout << \"Default constructor called\" << std::endl;\n"
+            cppFile += "} \n\n"
+            cppFile += value + "::~" + value + "(){\n"
+            cppFile += "\t" + "std::cout << \"Destructor called\" << std::endl;\n"
+            cppFile += "} \n\n"
+            cppFile += value + "::" + value + "(const " + value + " &" + value[0].lower() + "){\n"
+            cppFile += "\t" + "std::cout << \"Copy constructor called\" << std::endl;\n"
+            cppFile += "\t *this = " + value[0].lower() + ";\n"
+            cppFile += "} \n\n"
+            cppFile += value + " & "  + value + "::operator =" + "(const " + value + " &" + value[0].lower() + "){\n"
+            cppFile += "\t" + "std::cout << \"Copy assignment operator called\" << std::endl;\n"
+            cppFile += "\tif(this != &" + value[0].lower() + ")\n"
+            cppFile += "\t{ \n"
+            cppFile += "\t} \n"
+            cppFile += "\treturn *this;\n"
+            cppFile += "} \n\n"
+            f.write(cppFile)
+
 if (len( sys.argv ) > 1):
-    className = sys.argv[1]
-# generate header file
-    with open(className + '.hpp', 'w') as f:
-        headerFile = "#ifndef " + className.upper() + "_CLASS\n"
-        headerFile += "#define " + className.upper() + "_CLASS\n\n"
-        headerFile += "#include <iostream>\n\n"
-        headerFile += "class " + className + "\n"
-        headerFile += "{\n"
-        headerFile += "public:\n"
-        headerFile += "\t" + className + "();\n"
-        headerFile += "\t" + "~" + className + "();\n"
-        headerFile += "\t" + className + "(const " + className + " &" + className[0].lower() + ");\n"
-        headerFile += "\t" + className + " & operator =" + "(const " + className + " &" + className[0].lower() + ");\n"
-        headerFile += "private: \n\n"
-        headerFile += "}; \n\n"
-        headerFile += "#endif // !" + className.upper() + "_CLASS \n"
-        f.write(headerFile)
-# generate cpp file
-    with open(className + '.cpp', 'w') as f:
-        cppFile = "#include \"" + className + ".hpp\"\n\n"
-        cppFile += className + "::" + className + "(){\n"
-        cppFile += "\t" + "std::cout << \"Default constructor called\" << std::endl;\n"
-        cppFile += "} \n\n"
-        cppFile += className + "::~" + className + "(){\n"
-        cppFile += "\t" + "std::cout << \"Destructor called\" << std::endl;\n"
-        cppFile += "} \n\n"
-        cppFile += className + "::" + className + "(const " + className + " &" + className[0].lower() + "){\n"
-        cppFile += "\t" + "std::cout << \"Copy constructor called\" << std::endl;\n"
-        cppFile += "\t *this = " + className[0].lower() + ";\n"
-        cppFile += "} \n\n"
-        cppFile += className + " & "  + className + "::operator =" + "(const " + className + " &" + className[0].lower() + "){\n"
-        cppFile += "\t" + "std::cout << \"Copy assignment operator called\" << std::endl;\n"
-        cppFile += "\tif(this != &" + className[0].lower() + ")\n"
-        cppFile += "\t{ \n"
-        cppFile += "\t} \n"
-        cppFile += "\treturn *this;\n"
-        cppFile += "} \n\n"
-        f.write(cppFile)
-else:
-    print("please provide class Name")
+    GenerateClass(sys.argv[1].split(','))
